13fd8ac14719ab9766fc4c39dc96fee8783e7d16
thom364e <thomas.jensen@hotmail.dk>
2024-10-21 17:33:54 +0200

    added working partial equilibrium model

--- notebooks/hh_housing.py
+++ notebooks/hh_housing.py
@@ -9,105 +9,105 @@ from numba import njit
 from sequence_jacobian.blocks.het_block import het
 from sequence_jacobian import interpolate, grids
 
-def hh_init(b_grid, a_grid, z_grid, eis):
-    Va = (0.6 + 1.1 * b_grid[:, np.newaxis] + a_grid) ** (-1 / eis) * np.ones((z_grid.shape[0], 1, 1))
-    Vb = (0.5 + b_grid[:, np.newaxis] + 1.2 * a_grid) ** (-1 / eis) * np.ones((z_grid.shape[0], 1, 1))
-    return Va, Vb
+def hh_init(b_bhat_grid, a_bhat_grid, z_grid, eis):
+    Va_bhat = (0.6 + 1.1 * b_bhat_grid[:, np.newaxis] + a_bhat_grid) ** (-1 / eis) * np.ones((z_grid.shape[0], 1, 1))
+    Vb_bhat = (0.5 + b_bhat_grid[:, np.newaxis] + 1.2 * a_bhat_grid) ** (-1 / eis) * np.ones((z_grid.shape[0], 1, 1))
+    return Va_bhat, Vb_bhat
 
 
-def adjustment_costs(a, a_grid, ra, chi0, chi1, chi2):
-    chi = get_Psi_and_deriv(a, a_grid, ra, chi0, chi1, chi2)[0]
+def adjustment_costs(a_bhat, a_bhat_grid, ra, chi0, chi1, chi2):
+    chi = get_Psi_and_deriv(a_bhat, a_bhat_grid, ra, chi0, chi1, chi2)[0]
     return chi
 
 
-def marginal_cost_grid(a_grid, ra, chi0, chi1, chi2):
+def marginal_cost_grid(a_bhat_grid, ra, chi0, chi1, chi2):
     # precompute Psi1(a', a) on grid of (a', a) for steps 3 and 5
-    Psi1 = get_Psi_and_deriv(a_grid[:, np.newaxis],
-                             a_grid[np.newaxis, :], ra, chi0, chi1, chi2)[1]
+    Psi1 = get_Psi_and_deriv(a_bhat_grid[:, np.newaxis],
+                             a_bhat_grid[np.newaxis, :], ra, chi0, chi1, chi2)[1]
     return Psi1
 
 
 # policy and bacward order as in grid!
-@het(exogenous='Pi', policy=['b', 'a'], backward=['Vb', 'Va'],
+@het(exogenous='Pi', policy=['b_bhat', 'a_bhat'], backward=['Vb_bhat', 'Va_bhat'],
      hetinputs=[marginal_cost_grid], hetoutputs=[adjustment_costs], backward_init=hh_init)  
-def hh(Va_p, Vb_p, a_grid, b_grid, z_grid, e_grid, k_grid, beta, eis, rb, ra, chi0, chi1, chi2, Psi1):
+def hh(Va_bhat_p, Vb_bhat_p, a_bhat_grid, b_bhat_grid, z_grid, e_grid, k_grid, beta, eis, rb, ra, chi0, chi1, chi2, Psi1, gamma):
     # === STEP 2: Wb(z, b', a') and Wa(z, b', a') ===
     # (take discounted expectation of tomorrow's value function)
-    Wb = beta * Vb_p
-    Wa = beta * Va_p
+    Wb = beta * Vb_bhat_p
+    Wa = beta * Va_bhat_p
     W_ratio = Wa / Wb
 
     # === STEP 3: a'(z, b', a) for UNCONSTRAINED ===
 
     # for each (z, b', a), linearly interpolate to find a' between gridpoints
-    # satisfying optimality condition W_ratio == 1+Psi1
-    i, pi = lhs_equals_rhs_interpolate(W_ratio, 1 + Psi1)
+    # satisfying optimality condition W_ratio == 1 - gamma + Psi1
+    i, pi = lhs_equals_rhs_interpolate(W_ratio, (1 - gamma) + Psi1)
 
     # use same interpolation to get Wb and then c
-    a_endo_unc = interpolate.apply_coord(i, pi, a_grid)
+    a_endo_unc = interpolate.apply_coord(i, pi, a_bhat_grid)
     c_endo_unc = interpolate.apply_coord(i, pi, Wb) ** (-eis)
     
     # === STEP 4: b'(z, b, a), a'(z, b, a) for UNCONSTRAINED ===
 
     # solve out budget constraint to get b(z, b', a)
-    b_endo = (c_endo_unc + a_endo_unc + addouter(-z_grid, b_grid, -(1 + ra) * a_grid)
-              + get_Psi_and_deriv(a_endo_unc, a_grid, ra, chi0, chi1, chi2)[0]) / (1 + rb)
+    b_endo = (c_endo_unc + (1-gamma)*a_endo_unc + addouter(-z_grid, b_bhat_grid, ((1 + rb)*gamma - (1 + ra)) * a_bhat_grid)
+              + get_Psi_and_deriv(a_endo_unc, a_bhat_grid, ra, chi0, chi1, chi2)[0]) / (1 + rb)
 
     # interpolate this b' -> b mapping to get b -> b', so we have b'(z, b, a)
     # and also use interpolation to get a'(z, b, a)
     # (note utils.interpolate.interpolate_coord and utils.interpolate.apply_coord work on last axis,
     #  so we need to swap 'b' to the last axis, then back when done)
-    i, pi = interpolate.interpolate_coord(b_endo.swapaxes(1, 2), b_grid)
+    i, pi = interpolate.interpolate_coord(b_endo.swapaxes(1, 2), b_bhat_grid)
     a_unc = interpolate.apply_coord(i, pi, a_endo_unc.swapaxes(1, 2)).swapaxes(1, 2)
-    b_unc = interpolate.apply_coord(i, pi, b_grid).swapaxes(1, 2)
+    b_unc = interpolate.apply_coord(i, pi, b_bhat_grid).swapaxes(1, 2)
 
     # === STEP 5: a'(z, kappa, a) for CONSTRAINED ===
 
     # for each (z, kappa, a), linearly interpolate to find a' between gridpoints
     # satisfying optimality condition W_ratio/(1+kappa) == 1+Psi1, assuming b'=0
     lhs_con = W_ratio[:, 0:1, :] / (1 + k_grid[np.newaxis, :, np.newaxis])
-    i, pi = lhs_equals_rhs_interpolate(lhs_con, 1 + Psi1)
+    i, pi = lhs_equals_rhs_interpolate(lhs_con, (1 - gamma) + Psi1)
 
     # use same interpolation to get Wb and then c
-    a_endo_con = interpolate.apply_coord(i, pi, a_grid)
+    a_endo_con = interpolate.apply_coord(i, pi, a_bhat_grid)
     c_endo_con = ((1 + k_grid[np.newaxis, :, np.newaxis]) ** (-eis)
                   * interpolate.apply_coord(i, pi, Wb[:, 0:1, :]) ** (-eis))
 
     # === STEP 6: a'(z, b, a) for CONSTRAINED ===
 
     # solve out budget constraint to get b(z, kappa, a), enforcing b'=0
-    b_endo = (c_endo_con + a_endo_con
-              + addouter(-z_grid, np.full(len(k_grid), b_grid[0]), -(1 + ra) * a_grid)
-              + get_Psi_and_deriv(a_endo_con, a_grid, ra, chi0, chi1, chi2)[0]) / (1 + rb)
+    b_endo = (c_endo_con + (1-gamma)*a_endo_con
+              + addouter(-z_grid, np.full(len(k_grid), b_bhat_grid[0]), ((1 + rb)*gamma - (1 + ra)) * a_bhat_grid)
+              + get_Psi_and_deriv(a_endo_con, a_bhat_grid, ra, chi0, chi1, chi2)[0]) / (1 + rb)
 
     # interpolate this kappa -> b mapping to get b -> kappa
     # then use the interpolated kappa to get a', so we have a'(z, b, a)
     # (utils.interpolate.interpolate_y does this in one swoop, but since it works on last
     #  axis, we need to swap kappa to last axis, and then b back to middle when done)
-    a_con = interpolate.interpolate_y(b_endo.swapaxes(1, 2), b_grid,
+    a_con = interpolate.interpolate_y(b_endo.swapaxes(1, 2), b_bhat_grid,
                                       a_endo_con.swapaxes(1, 2)).swapaxes(1, 2)
 
     # === STEP 7: obtain policy functions and update derivatives of value function ===
 
     # combine unconstrained solution and constrained solution, choosing latter
     # when unconstrained goes below minimum b
-    a, b = a_unc.copy(), b_unc.copy()
-    b[b <= b_grid[0]] = b_grid[0]
-    a[b <= b_grid[0]] = a_con[b <= b_grid[0]]
+    a_bhat, b_bhat = a_unc.copy(), b_unc.copy()
+    b_bhat[b_bhat <= b_bhat_grid[0]] = b_bhat_grid[0]
+    a_bhat[b_bhat <= b_bhat_grid[0]] = a_con[b_bhat <= b_bhat_grid[0]]
 
     # calculate adjustment cost and its derivative
-    Psi, _, Psi2 = get_Psi_and_deriv(a, a_grid, ra, chi0, chi1, chi2)
+    Psi, _, Psi2 = get_Psi_and_deriv(a_bhat, a_bhat_grid, ra, chi0, chi1, chi2)
 
     # solve out budget constraint to get consumption and marginal utility
-    c = addouter(z_grid, (1 + rb) * b_grid, (1 + ra) * a_grid) - Psi - a - b
-    uc = c ** (-1 / eis)
-    uce = e_grid[:, np.newaxis, np.newaxis] * uc
+    c_bhat = addouter(z_grid, (1 + rb) * b_bhat_grid, -((1 + rb)*gamma - (1 + ra)) * a_bhat_grid) - Psi - (1-gamma)*a_bhat - b_bhat
+    uc = c_bhat ** (-1 / eis)
+    uce_bhat = e_grid[:, np.newaxis, np.newaxis] * uc
 
     # update derivatives of value function using envelope conditions
-    Va = (1 + ra - Psi2) * uc
-    Vb = (1 + rb) * uc
+    Va_bhat = (1 + ra - (1 + rb)*gamma - Psi2) * uc
+    Vb_bhat = (1 + rb) * uc
 
-    return Va, Vb, a, b, c, uce
+    return Va_bhat, Vb_bhat, a_bhat, b_bhat, c_bhat, uce_bhat
 
 def hh_sandbox(Va_p, Vb_p, a_grid, b_grid, bhat_grid, b_endo_2d_grid, z_grid, e_grid, k_grid, beta, eis, rb, ra, chi0, chi1, chi2, gamma, Psi1, debug=False):
     # === STEP 2: Wb(z, b', a') and Wa(z, b', a') ===
